93f21a3c55cea806f10ec3eddfe94be4e24ed8d5
ValentinaZussino <vzussino@protonmail.com>
2022-11-25 13:55:27 +0100

    inizio impostazione files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prova PHP form</title>
+</head>
+<body>
+    <form action="ricezione.php" method="GET">
+        <input type="text" name="paragrafo" placeholder="Immetti un testo">
+        <input type="text" name="parola" placeholder="Parola da censurare">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
---
+++ ricezione.php
@@ -0,0 +1,18 @@
+<?php
+$paragrafo = $_GET['paragrafo'];
+$parola = $_GET['parola'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ricezione Form</title>
+</head>
+<body>
+    <p><?php echo $paragrafo; ?></p>
+    <p><?php echo $parola; ?></p>
+</body>
+</html>
